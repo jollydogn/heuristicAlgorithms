cd0214c17fbe64034ecfd163818436a0c1a936c7
Test user <test@testuser.com>
2022-11-13 01:19:23 +0300

    Added SA

--- SA/SA.py
+++ SA/SA.py
@@ -90,20 +90,11 @@ def simulated_annealing_geometric(min_values = [-5,-5], max_values = [5,5], mu =
     best  = np.copy(guess)
     fx_best = guess[0,-1]
     Temperature = float(initial_temperature)
-    step = 1
     
-    excelstep = []
-    exceltemp = []
-    excelfx = []
-    excelbest = []
     while (Temperature > final_temperature): 
         # for repeat in range(0, temperature_iterations):
         if (verbose == True):
-            print('Step = ',step ,' Temperature = ', round(Temperature, 4), ' f(x) = ', round(best[0, -1], 4))
-            
-            excelstep.append(step)
-            exceltemp.append(round(Temperature, 4)) 
-            excelfx.append(round(best[0, -1], 4)) 
+            print(' Temperature = ', round(Temperature, 4), ' f(x) = ', round(best[0, -1], 4))
             
             fx_old    =  guess[0,-1]    
             epson     = epson_vector(guess, mu = mu, sigma = sigma)
@@ -114,8 +105,6 @@ def simulated_annealing_geometric(min_values = [-5,-5], max_values = [5,5], mu =
             p         = np.exp(-delta/Temperature)
             print('Best solution vector;\n',best,'\n')
             
-            excelbest.append(best) 
-            step += 1
         if (delta < 0 or r <= p):
                 guess = np.copy(new_guess)   
         if (fx_new < fx_best):
@@ -123,91 +112,6 @@ def simulated_annealing_geometric(min_values = [-5,-5], max_values = [5,5], mu =
         best    = np.copy(guess)
         Temperature = alpha*Temperature   
         return best
-        
-        
-    # for i in excelbest:
-    #     print(i)
-    
-        if (initial_temperature == 1000):
-            
-            wb = load_workbook(r'C:/Users/Emirhan/Desktop/OKUL/4.SINIF 1.DÖNEM/INTRODUCTION TO HEURISTIC ALGORITHMS/HAFTA 3/SA - Ödev/SA/Results.xlsx')  
-            sheet = wb.active 
-            
-            sheet.cell(row=1, column=1).value = 'Geometric - Init temp 1000'
-            sheet.cell(row=step+1, column=1).value = 'Step'
-            sheet.cell(row=step+1, column=2).value = 'Temperature' 
-            sheet.cell(row=step+1, column=3).value = 'f(x)' 
-            sheet.cell(row=2, column=4).value = 'Best'
-            
-            for a in range(len(excelstep)):
-                temp = excelstep[a]
-                sheet.cell(row=a+3, column=1).value = temp
-                
-            for b in range(len(exceltemp)):
-                temp = exceltemp[b]
-                sheet.cell(row=b+3, column=2).value = temp
-                
-            for c in range(len(excelfx)):
-                temp = excelfx[c]
-                sheet.cell(row=c+3, column=3).value = temp
-            
-            wb.save(r'C:/Users/Emirhan/Desktop/OKUL/4.SINIF 1.DÖNEM/INTRODUCTION TO HEURISTIC ALGORITHMS/HAFTA 3/SA - Ödev/SA/Results.xlsx')  
-            
-            wb.close()
-            
-        elif (initial_temperature == 5000):
-            
-            wb = load_workbook(r'C:/Users/Emirhan/Desktop/OKUL/4.SINIF 1.DÖNEM/INTRODUCTION TO HEURISTIC ALGORITHMS/HAFTA 3/SA - Ödev/SA/Results.xlsx')  
-            sheet = wb.active 
-            
-            sheet.cell(row=1, column=7).value = 'Geometric - Init temp 5000'
-            sheet.cell(row=step+1, column=7).value = 'Step'
-            sheet.cell(row=step+1, column=8).value = 'Temperature' 
-            sheet.cell(row=step+1, column=9).value = 'f(x)' 
-            sheet.cell(row=2, column=10).value = 'Best'
-            
-            for a in range(len(excelstep)):
-                temp = excelstep[a]
-                sheet.cell(row=a+3, column=7).value = temp
-                
-            for b in range(len(exceltemp)):
-                temp = exceltemp[b]
-                sheet.cell(row=b+3, column=8).value = temp
-                
-            for c in range(len(excelfx)):
-                temp = excelfx[c]
-                sheet.cell(row=c+3, column=9).value = temp
-            
-            wb.save(r'C:/Users/Emirhan/Desktop/OKUL/4.SINIF 1.DÖNEM/INTRODUCTION TO HEURISTIC ALGORITHMS/HAFTA 3/SA - Ödev/SA/Results.xlsx')  
-            
-            wb.close()
-            
-        elif (initial_temperature == 10000):
-            
-            wb = load_workbook(r'C:/Users/Emirhan/Desktop/OKUL/4.SINIF 1.DÖNEM/INTRODUCTION TO HEURISTIC ALGORITHMS/HAFTA 3/SA - Ödev/SA/Results.xlsx')  
-            sheet = wb.active 
-            
-            sheet.cell(row=1, column=13).value = 'Geometric - Init temp 10000'
-            sheet.cell(row=step+1, column=13).value = 'Step'
-            sheet.cell(row=step+1, column=14).value = 'Temperature' 
-            sheet.cell(row=step+1, column=15).value = 'f(x)' 
-            sheet.cell(row=2, column=16).value = 'Best'
-            
-            for a in range(len(excelstep)):
-                temp = excelstep[a]
-                sheet.cell(row=a+3, column=13).value = temp
-                
-            for b in range(len(exceltemp)):
-                temp = exceltemp[b]
-                sheet.cell(row=b+3, column=14).value = temp
-                
-            for c in range(len(excelfx)):
-                temp = excelfx[c]
-                sheet.cell(row=c+3, column=15).value = temp
-            
-            wb.save(r'C:/Users/Emirhan/Desktop/OKUL/4.SINIF 1.DÖNEM/INTRODUCTION TO HEURISTIC ALGORITHMS/HAFTA 3/SA - Ödev/SA/Results.xlsx')  
-            
-            wb.close()
 
 # SA Lineer
 def simulated_annealing_linear(min_values = [-5,-5], max_values = [5,5], mu = 0, sigma = 1, initial_temperature = 1000.0, temperature_iterations = 1000, final_temperature = 0.0001, alpha = 0.9, target_function = target_function, verbose = True):    
@@ -236,5 +140,6 @@ def simulated_annealing_linear(min_values = [-5,-5], max_values = [5,5], mu = 0,
         # Temperature = alpha*Temperature   
         # return best
         Temperature = Temperature - alpha
+        return best
     
 ############################################################################
--- SA/run.py
+++ SA/run.py
@@ -8,14 +8,8 @@ import array
 import pandas as pd
 import time
 import statistics as stat
-
-def SimulatedAnnealingLinear():
-    obj_func = functions.selectFunction(Functions.ackley)
-    # dim array size, -5 lb +5 lb 
-    simulated_annealing_linear( min_values = [-32768,-32768,-32768,-32768,-32768], max_values = [32768,32768,32768,32768,32768], mu = 0, sigma = 1, initial_temperature = 100.0, temperature_iterations = 100,
-        final_temperature = 0.0001, alpha = 0.9, target_function = obj_func, verbose = True)
     
-def SimulatedAnnealingGeometric():
+def SimulatedAnnealing():
     objective_functions=[functions.selectFunction(Functions.ackley),functions.selectFunction(Functions.griewank),functions.selectFunction(Functions.schwefel),functions.selectFunction(Functions.rastrigin),functions.selectFunction(Functions.sphere),functions.selectFunction(Functions.perm),functions.selectFunction(Functions.zakharov),functions.selectFunction(Functions.rosenbrock),functions.selectFunction(Functions.damavandi)]
     bounds=[[-32768,32768],[-600,600],[-500,500],[-5.12,5.12],[-5.12,5.12],[-30,30],[-5,10],[-2048,2048],[0,14]]
     temperatures = [1000,5000,10000]
@@ -30,20 +24,34 @@ def SimulatedAnnealingGeometric():
             upper = np.empty(30)
             upper.fill(bound[1])
             
-            for _ in range(5):
-                best = simulated_annealing_geometric( min_values = lower, max_values = upper, mu = 0, sigma = 1, initial_temperature = temp, temperature_iterations = 100,
-                final_temperature = 0.0001, alpha = 0.9, target_function = obj_func, verbose = True)
-                results.append([obj_func.__name__,temp,best[0][29]])
+            mode=[0,1]
+            for idx,mode in enumerate(mode):
+                for _ in range(5):
+                    if(mode==0):
+                        best = simulated_annealing_geometric( min_values = lower, max_values = upper, mu = 0, sigma = 1, initial_temperature = temp, temperature_iterations = 100,
+                        final_temperature = 0.0001, alpha = 0.9, target_function = obj_func, verbose = True)
+                        results.append([obj_func.__name__,temp,round(best[0, -1], 4),'geometric'])
+                    else:
+                        best = simulated_annealing_linear( min_values = lower, max_values = upper, mu = 0, sigma = 1, initial_temperature = temp, temperature_iterations = 100,
+                        final_temperature = 0.0001, alpha = 0.9, target_function = obj_func, verbose = True)
+                        results.append([obj_func.__name__,temp,round(best[0, -1], 4),'linear'])
+
+
             #my_array = np.array([[obj_func,temp,best[0][29]]])
             #df1 = pd.DataFrame(my_array, columns = ['obj_f','temp','geometric'])
-            df=pd.DataFrame(results,columns = ['obj_f','temp','geometric'])
+            df=pd.DataFrame(results,columns = ['obj_f','temp','best','type'])
             #df['std_dev'] = df.apply(lambda _: '', axis=1)
            # df.iloc[-1]['std_dev']=df['geometric'].std()
             # df.iloc[-1:]['std_dev']=df['geometric'].std()
             
-            df['std_dev']=df['geometric'].std()
-            df['avg_fitness']=df['geometric'].mean()
-            df['max_fitness']=df['geometric'].max()
+            # df['std_dev']=df['best'].std()
+            # df['avg_fitness']=df['best'].mean()
+            # df['max_fitness']=df['best'].max()
+            # dfs[index] = df
+            
+            df['std_dev'] = df['best'].rolling(5).std()
+            df['avg_fitness']=df['best'].rolling(5).mean()
+            df['max_fitness']=df['best'].rolling(5).min()
             dfs[index] = df
 
         results=[]
@@ -59,14 +67,13 @@ def SimulatedAnnealingGeometric():
             # SimulatedAnnealingGeometric(obj_func,bound[0],bound[1],30,temp)
             
             
-    
-    
-
 def main():
     # temperatures = [1000,5000,10000]
     # for i in temperatures:
     #     SimulatedAnnealingGeometric(i)
-    SimulatedAnnealingGeometric()
+    SimulatedAnnealing()
+    
+    
 
 if __name__ == "__main__":
     main()
